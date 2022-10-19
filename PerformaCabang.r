efc3881372c882bac59c7a1c64d06898dccd4c5b
Eric Leonardo <96973852+ericleo8@users.noreply.github.com>
2022-10-19 23:50:34 +0700

    Update PerformaCabang.r

--- PerformaCabang.r
+++ PerformaCabang.r
@@ -69,3 +69,30 @@ df_loan_mei_flag %>%
             total_amount = sum(amount)) %>% 
   arrange(total_amount) %>% 
   mutate_if(is.numeric, funs(comma))
+
+library(dplyr)
+library(scales)
+df_loan_mei_flag %>% 
+  filter(umur == 3, flag == 'rendah') %>% 
+  inner_join(df_loan, by = 'cabang') %>% 
+  filter(tanggal_cair >= '2020-05-01', tanggal_cair<= '2020-05-31') %>% 
+  group_by(cabang, agen) %>% 
+  summarise(jumlah_hari = n_distinct(tanggal_cair),
+            total_loan_cair = n_distinct(loan_id),
+            avg_amount = mean(amount), 
+            total_amount = sum(amount)) %>% 
+  arrange(total_amount) %>% 
+  mutate_if(is.numeric, funs(comma))
+
+library(dplyr)
+library(scales)
+df_loan %>% 
+  filter(cabang == 'AH') %>% 
+  filter(tanggal_cair >= '2020-05-01', tanggal_cair <- '2020-05-31') %>% 
+  group_by(cabang, agen) %>% 
+  summarise(jumlah_hari = n_distinct(tanggal_cair),
+            total_loan_cair = n_distinct(loan_id),
+            avg_amount = mean(amount), 
+            total_amount = sum(amount)) %>% 
+  arrange(total_amount) %>% 
+  mutate_if(is.numeric, funs(comma))
