6f752115f8bab3843daa79ec9267d07cf839dafd
Eric Leonardo <96973852+ericleo8@users.noreply.github.com>
2022-10-19 23:14:45 +0700

    Update PerformaCabang.r

--- PerformaCabang.r
+++ PerformaCabang.r
@@ -43,3 +43,29 @@ ggplot(df_loan_mei_umur, aes(x = umur, y = total_amount)) +
        x = "Umur(bulan)",
        y = "Total Amount")
 
+library(ggplot2)
+
+ggplot(df_loan_mei_flag, aes(x = umur, y = total_amount)) +
+  geom_point(aes(color = flag)) +
+  scale_color_manual(breaks = c("baik", "rendah"),
+                        values=c("blue", "red")) +
+  scale_y_continuous(labels = scales::comma) +
+  labs(title= "Ada cabang berpeforma rendah padahal tidak termasuk bottom 5 nasional",
+       color = "",
+       x = "Umur (bulan)",
+       y = "Total Amount")
+
+library(dplyr)
+library(scales)
+df_loan_mei_flag %>% 
+  filter(umur == 3) %>% 
+  inner_join(df_loan, by = 'cabang') %>% 
+  filter(tanggal_cair >= '2020-05-01', tanggal_cair <= '2020-05-31') %>% 
+  group_by(cabang, flag) %>% 
+  summarise(jumlah_hari = n_distinct(tanggal_cair),
+            agen_aktif = n_distinct(agen),
+            total_loan_cair = n_distinct(loan_id),
+            avg_amount = mean(amount), 
+            total_amount = sum(amount)) %>% 
+  arrange(total_amount) %>% 
+  mutate_if(is.numeric, funs(comma))
